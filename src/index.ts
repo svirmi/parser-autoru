c8c41908881e25edcf541327bd931219a59a604c
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-02-02 14:51:19 +0200

    initial setup

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import cookie from "./helpers/cookie";
             const browser = await puppeteer.launch({args: ['--no-sandbox']});
             const page = await browser.newPage();
             await page.setCookie(...cookie);
-            await page.goto('https://auto.ru/catalog/cars/all/?page_num=1', {waitUntil: 'networkidle2'});
+            await page.goto('https://www.gearbest.com/', {waitUntil: 'networkidle2'});
             await page.waitFor(1000);
             await page.screenshot({path: './data/example.png'});
             await browser.close();
