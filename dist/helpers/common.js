6f6443f3131e905a26976cca9a79afe4d69425ac
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-02-02 14:56:41 +0200

    initial setup

--- dist/helpers/common.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function arrayFromLength(number) {
-    return Array.from(new Array(number).keys()).map(function (k) { return k + 1; });
-}
-exports.arrayFromLength = arrayFromLength;
